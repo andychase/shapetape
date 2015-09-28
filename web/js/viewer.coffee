7c83c8686815088945d7ce8e03005ab3a277fa04
Andy Chase <asperous2@gmail.com>
2015-09-27 20:18:25 -0700

    Implement a simple viewer

---
+++ web/js/viewer.coffee
@@ -0,0 +1,47 @@
+$ ->
+  img_container = $('#img-container')
+  vector = $('#vector')
+  placeholder = $('#placeholder')
+  $('#vector, #placeholder, .small-logo').on 'dragstart', (event) ->
+    event.preventDefault()
+
+  toggleImg = ->
+    placeholder.show()
+    vector.hide()
+  toggleVec = ->
+    placeholder.hide()
+    vector.show()
+  toggleVec()
+
+
+  slideValue = 90
+  zooming = false
+  $('.slider').on 'input', ->
+    if not zooming
+      toggleImg()
+      zooming = true
+    slideValue = this.value
+    img_container.css('width', slideValue + "%")
+
+  $('.slider').on 'mouseup', ->
+    if zooming
+      toggleVec()
+      zooming = false
+    return true
+
+  curYPos = 0
+  curXPos = 0
+  curDown = false
+  window.addEventListener 'mousemove', (e) ->
+    if not zooming and curDown
+      window.scrollTo document.body.scrollLeft + curXPos - (e.pageX), document.body.scrollTop + curYPos - (e.pageY)
+    return true
+  window.addEventListener 'mousedown', (e) ->
+    if not zooming and e.clientY > 65
+      curDown = true
+      curYPos = e.pageY
+      curXPos = e.pageX
+    return true
+  window.addEventListener 'mouseup', (e) ->
+    curDown = false
+    return true
---
+++ web/viewer.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Shapetape - Viewer</title>
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css">
+    <style type='text/css'>
+        html, body, img {
+            -webkit-touch-callout: none;
+            -webkit-user-select: none;
+            -khtml-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        body {
+            height: 100%;
+            width: 100%;
+            margin: 0;
+        }
+
+        .small-logo {
+            width: 300px;
+            padding-left: 10px;
+            padding-right: 10px;
+            padding-bottom: 5px;
+            padding-top: 5px;
+        }
+
+        #img-container {
+            position: absolute;
+            width: 90%;
+            left: 0;
+            top: 61px;
+        }
+
+        #img-container img {
+            display: block;
+            width: 100%;
+            height: 100%;
+        }
+
+        .top {
+            z-index: 300;
+            background-color: rgba(255, 255, 255, 1);
+            position: fixed;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 60px;
+            border-bottom: 1px solid black;
+        }
+
+        .top label {
+            position: relative;
+            bottom: 18%;
+        }
+    </style>
+</head>
+<body>
+<div class="top">
+    <img src="img/small_logo.svg" class='small-logo'>
+    <label>Zoom:<input class="slider" type="range" min="90" max="200" step="1" value="100"/></label>
+</div>
+
+<div id="img-container">
+    <img src="img/test.png" id='placeholder'>
+    <img src="img/test.svg" id='vector'>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="js/viewer.js"></script>
+</body>
+</html>
